62b34cabadc890ed94e6f952c999252469145bfd
Sameen Shakeel <49653224+SameenShakeel@users.noreply.github.com>
2020-06-20 23:30:24 +0500

    Added Functionality

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
-export const Transaction = () => {
+export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
         <div>
-           <li className="minus">
-                    {transaction.text} <span>-400 Rs</span>
-                    <button className="delete-btn">x</button>
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+                {transaction.text} <span>{sign}{Math.abs(transaction.amount)} Rs</span>
+                <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
             </li>
         </div>
     )
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -10,7 +10,7 @@ export const TransactionList = () => {
         <div>
             <h3>Transaction History</h3>
             <ul className="list">
-                {transactions.map(transaction => (<Transaction />))}
+                {transactions.map(transaction => (<Transaction key={ transaction.id } transaction={ transaction }/>))}
             </ul>
         </div>
     );
