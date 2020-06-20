3668d127081c7a5322a24e99244722e3d55e7bf4
Sameen Shakeel <49653224+SameenShakeel@users.noreply.github.com>
2020-06-20 23:46:26 +0500

    Updated App.css

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -9,7 +9,7 @@ export const Balance = () => {
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
     return (
-        <div>
+        <div className="blnc">
             <h4>Your Balance</h4>
             <h1>{total} Rs</h1>
         </div>
